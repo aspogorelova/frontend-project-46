7b034785619f246aacde850f3e4f67af67d3d81d
Анна Погорелова <anyapogorelova17@gmail.com>
2023-03-19 10:15:06 +0300

    fix: corrected gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,14 +9,4 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1');
 
-// program.command('compare')
-//   .description('Compares two configuration files and shows a difference.')
-//   .argument('<filepath1>', '<filepath2>')
-//   .option('-V, --version', 'output the version number')
-//   .option('-h, --help', 'display help for command')
-  // .action((str, options) => {
-  //   const limit = options.first ? 1 : undefined;
-  //   console.log(str.split(options.separator, limit));
-  // });
-
 program.parse();
