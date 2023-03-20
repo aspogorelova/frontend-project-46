15f737b365560afe574d9c2ed3fc4ef1b48a1985
Анна Погорелова <anyapogorelova17@gmail.com>
2023-03-20 14:46:12 +0300

    feat: add utils files

---
+++ src/read.js
@@ -0,0 +1,14 @@
+import path from 'path';
+import { readFileSync } from 'fs';
+import { cwd } from 'process';
+
+const getPathFile = (file) => path.resolve(cwd(), file);
+
+const readFile = (file) => {
+  const path = getPathFile(file);
+  const data = readFileSync(path, 'utf-8');
+  return JSON.parse(data);
+}
+
+export default readFile;
+// console.log(readFile('file1.json'));
