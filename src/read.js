6efbb43cd1d67aef731138e4cf2c953d8432bcc3
Анна Погорелова <anyapogorelova17@gmail.com>
2023-03-23 16:06:51 +0300

    fix: corrected eslint mistake

--- src/read.js
+++ src/read.js
@@ -1,18 +1,16 @@
 import path from 'path';
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const getPathFile = (filename) => path.join(__dirname, '..', '__tests__', '__fixtures__', filename);
 
 const readFile = (file) => {
-  const path = getPathFile(file);
-  const data = readFileSync(path, 'utf-8');
+  const pathOfFile = getPathFile(file);
+  const data = readFileSync(pathOfFile, 'utf-8');
   return JSON.parse(data);
-}
+};
 
 export default readFile;
-console.log(readFile('file1.json'));
