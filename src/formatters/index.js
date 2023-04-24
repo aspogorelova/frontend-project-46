915d2b543d1fb70dbcdb94f04a20ebda4e7427a6
aspogorelova <anyapogorelova17@gmail.com>
2023-04-24 13:55:54 +0300

    feat: add new format - json

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,11 +1,14 @@
 import makeStylish from './stylish.js';
 import makePlain from './plain.js';
+import makeJson from './json.js';
 
 const makeFormatting = (treeDiff, format) => {
   switch (format) {
     case 'stylish': return makeStylish(treeDiff);
 
     case 'plain': return makePlain(treeDiff);
+
+    case 'json': return makeJson(treeDiff);
 
     default: return 'Unknown format';
   }
