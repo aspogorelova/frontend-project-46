5065fcc8c233e866b596f79340b5922a737fd7b8
aspogorelova <anyapogorelova17@gmail.com>
2023-05-17 18:06:55 +0300

    fix: update

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -4,19 +4,14 @@ import makePlain from './plain.js';
 const makeFormatting = (treeDiff, format) => {
   switch (format) {
     case 'stylish': return makeStylish(treeDiff);
-      break;
 
     case 'plain': return makePlain(treeDiff);
-      break;
 
     case 'json': return JSON.stringify(treeDiff);
-      break;
 
     default:
       throw new Error(`False format ${format}`);
   }
-
-  return format;
 };
 
 export default makeFormatting;
